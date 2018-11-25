4fbe219a9d849d2b22403cdd465aa4389713c746
Yevhen Vorobiov <yevhen.vorobiov@nure.ua>
2018-11-25 21:32:36 +0200

    partial implementation of HsqldbUserDao#create added

--- usermanagement/src/main/java/ua/nure/vorobiov/usermanagement/db/HsqldbUserDao.java
+++ usermanagement/src/main/java/ua/nure/vorobiov/usermanagement/db/HsqldbUserDao.java
@@ -2,10 +2,15 @@ package ua.nure.vorobiov.usermanagement.db;
 
 import ua.nure.vorobiov.usermanagement.User;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
 
 public class HsqldbUserDao implements UserDao {
 
+    private static final String INSERT_QUERY = "INSERT INTO users (firstname, lastname, dateofbirth) VALUES (?, ?, ?)";
     private ConnectionFactory connectionFactory;
 
     public HsqldbUserDao(ConnectionFactory connectionFactory) {
@@ -14,6 +19,20 @@ public class HsqldbUserDao implements UserDao {
 
     @Override
     public User create(User user) throws DatabaseException {
+        try {
+            Connection connection = connectionFactory.createConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY);
+            int count = 1;
+            preparedStatement.setString(count++, user.getFirstName());
+            preparedStatement.setString(count++, user.getLastName());
+            preparedStatement.setDate(count, (Date) user.getDateOfBirth());
+            int insertedRows = preparedStatement.executeUpdate();
+            if (insertedRows != 1) {
+                throw new DatabaseException("Number of the inserted rows: " + insertedRows);
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getMessage());
+        }
         return null;
     }
 
