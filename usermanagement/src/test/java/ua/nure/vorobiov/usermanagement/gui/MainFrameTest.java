0663ba99d451e74a82bcec356ceaaf8c10cf457e
Yevhen Vorobiov <yevhen.vorobiov@nure.ua>
2018-12-08 17:24:58 +0200

    MainFrameTest#testAddUser added

--- usermanagement/src/test/java/ua/nure/vorobiov/usermanagement/gui/MainFrameTest.java
+++ usermanagement/src/test/java/ua/nure/vorobiov/usermanagement/gui/MainFrameTest.java
@@ -3,9 +3,11 @@ package ua.nure.vorobiov.usermanagement.gui;
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
 import junit.extensions.jfcunit.TestHelper;
+import junit.extensions.jfcunit.eventdata.MouseEventData;
 import junit.extensions.jfcunit.finder.NamedComponentFinder;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import ua.nure.vorobiov.usermanagement.gui.main.MainFrame;
 
 import javax.swing.*;
@@ -15,6 +17,12 @@ public class MainFrameTest extends JFCTestCase {
 
     private MainFrame mainFrame;
 
+    private static final String ADD_PANEL = "addPanel";
+    private static final String FIRST_NAME_FIELD = "firstNameField";
+    private static final String LAST_NAME_FIELD = "lastNameField";
+    private static final String DATE_OF_BIRTH_FIELD = "dateOfBirthField";
+    private static final String OK_BUTTON = "okButton";
+    private static final String CANCEL_BUTTON = "cancelButton";
     private static final String BROWSE_PANEL = "browsePanel";
     private static final String USER_TABLE = "userTable";
     private static final String ADD_BUTTON = "addButton";
@@ -30,6 +38,7 @@ public class MainFrameTest extends JFCTestCase {
         mainFrame.setVisible(true);
     }
 
+    @Test
     public void testBrowseControls() {
         find(JPanel.class, BROWSE_PANEL);
         find(JTable.class, USER_TABLE);
@@ -37,6 +46,19 @@ public class MainFrameTest extends JFCTestCase {
         find(JButton.class, EDIT_BUTTON);
         find(JButton.class, DELETE_BUTTON);
         find(JButton.class, DETAILS_BUTTON);
+    }
+
+    @Test
+    public void testAddUser() {
+        JButton addButton = (JButton) find(JButton.class, ADD_BUTTON);
+        getHelper().enterClickAndLeave(new MouseEventData(this, addButton));
+        find(JPanel.class, ADD_PANEL);
+        find(JTextField.class, FIRST_NAME_FIELD);
+        find(JTextField.class, LAST_NAME_FIELD);
+        find(JTextField.class, DATE_OF_BIRTH_FIELD);
+        find(JButton.class, OK_BUTTON);
+        find(JButton.class, CANCEL_BUTTON);
+
     }
 
     @After
