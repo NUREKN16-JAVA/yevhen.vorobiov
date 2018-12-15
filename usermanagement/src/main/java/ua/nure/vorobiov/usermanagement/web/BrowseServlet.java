bcfa5a7908c43632a12bb86ffd23a9403e79c09d
Yevhen Vorobiov <yevhen.vorobiov@nure.ua>
2018-12-15 12:45:19 +0200

    BrowseServlet logic implemented.

--- usermanagement/src/main/java/ua/nure/vorobiov/usermanagement/web/BrowseServlet.java
+++ usermanagement/src/main/java/ua/nure/vorobiov/usermanagement/web/BrowseServlet.java
@@ -1,6 +1,30 @@
 package ua.nure.vorobiov.usermanagement.web;
 
+import ua.nure.vorobiov.usermanagement.User;
+import ua.nure.vorobiov.usermanagement.db.DaoFactory;
+import ua.nure.vorobiov.usermanagement.db.DatabaseException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
 
 public class BrowseServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        browse(req, resp);
+    }
+
+    private void browse(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Collection<User> users = DaoFactory.getInstance().getUserDao().findAll();
+            req.getSession(true).setAttribute("users", users);
+            req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+        } catch (DatabaseException e) {
+            throw new ServletException(e);
+        }
+    }
 }
