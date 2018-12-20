2d1e7dfff40b9f668195c32bb515d35fa37b1658
Yevhen Vorobiov <yevhen.vorobiov@nure.ua>
2018-12-20 17:09:37 +0200

    RequestServer#createReply implemented

--- usermanagement/src/main/java/ua/nure/vorobiov/usermanagement/agent/RequestServer.java
+++ usermanagement/src/main/java/ua/nure/vorobiov/usermanagement/agent/RequestServer.java
@@ -3,9 +3,13 @@ package ua.nure.vorobiov.usermanagement.agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import ua.nure.vorobiov.usermanagement.User;
+import ua.nure.vorobiov.usermanagement.db.DaoFactory;
+import ua.nure.vorobiov.usermanagement.db.DatabaseException;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
+import java.util.StringTokenizer;
 
 public class RequestServer extends CyclicBehaviour {
 
@@ -29,6 +33,31 @@ public class RequestServer extends CyclicBehaviour {
     }
 
     private ACLMessage createReply(ACLMessage message) {
-        return null;
+        ACLMessage reply = message.createReply();
+        String content = message.getContent();
+        StringTokenizer tokenizer = new StringTokenizer(content, ",");
+        if (tokenizer.countTokens() == 2) {
+            String firstName = tokenizer.nextToken();
+            String lastName = tokenizer.nextToken();
+            Collection<User> users;
+            try {
+                users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+            } catch (DatabaseException e) {
+                users = new ArrayList<>();
+                e.printStackTrace();
+            }
+            reply.setContent(convertToString(users));
+        }
+        return reply;
+    }
+
+    private String convertToString(Collection<User> users) {
+        StringBuilder builder = new StringBuilder();
+        users.forEach(user -> {
+            builder.append(user.getId()).append(",");
+            builder.append(user.getFirstName()).append(",");
+            builder.append(user.getLastName()).append(";");
+        });
+        return builder.toString();
     }
 }
