aeb3d0a887a29a53244c125a62d7172b26c454ec
Yevhen Vorobiov <yevhen.vorobiov@nure.ua>
2018-12-20 16:33:10 +0200

    SearchAgent#search added.

--- usermanagement/src/main/java/ua/nure/vorobiov/usermanagement/agent/SearchAgent.java
+++ usermanagement/src/main/java/ua/nure/vorobiov/usermanagement/agent/SearchAgent.java
@@ -1,7 +1,14 @@
 package ua.nure.vorobiov.usermanagement.agent;
 
 
+import org.dbunit.util.search.SearchException;
+
 import jade.core.Agent;
+import ua.nure.vorobiov.usermanagement.User;
+import ua.nure.vorobiov.usermanagement.db.DaoFactory;
+import ua.nure.vorobiov.usermanagement.db.DatabaseException;
+
+import java.util.Collection;
 
 public class SearchAgent extends Agent {
 
@@ -15,5 +22,21 @@ public class SearchAgent extends Agent {
     protected void takeDown() {
         System.out.println(getAID().getName() + " terminated");
         super.takeDown();
+    }
+
+    public void search(String firstName, String lastName) throws SearchException {
+        try {
+            Collection<User> users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+            if (users.size() > 0) {
+                showUsers(users);
+            } else {
+            }
+        } catch (DatabaseException e) {
+            throw new SearchException(e);
+        }
+    }
+
+    private void showUsers(Collection<User> users) {
+
     }
 }
